db53405a18ad2102758b1efba96f304ad8153636
David Wachlin <davidwachlin@gmail.com>
2019-12-28 16:17:34 -0500

    refactors custom map class constructor to take a divid argument

--- dist/src.f10117fe.js
+++ dist/src.f10117fe.js
@@ -127,8 +127,8 @@ Object.defineProperty(exports, "__esModule", {
 var CustomMap =
 /** @class */
 function () {
-  function CustomMap() {
-    this.googleMap = new google.maps.Map(document.getElementById('map'), {
+  function CustomMap(divId) {
+    this.googleMap = new google.maps.Map(document.getElementById(divId), {
       zoom: 1,
       center: {
         lat: 0,
@@ -151,7 +151,7 @@ Object.defineProperty(exports, "__esModule", {
 
 var CustomMap_1 = require("./CustomMap");
 
-new CustomMap_1.CustomMap();
+new CustomMap_1.CustomMap('map');
 },{"./CustomMap":"src/CustomMap.ts"}],"../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
